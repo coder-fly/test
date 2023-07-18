0203b6951c4b1cfb134eec98de3a9b80417fc58e
paseca <39791927+coder-fly@users.noreply.github.com>
2023-07-18 08:58:28 +0000

    v1

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ import aiohttp
 class Task:
     count = 0
     async def task(self):
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers={
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/114.0.1823.43',
+        }) as session:
             while True:
                 try:
                     async with session.get('https://github.com/coder-fly/douyin-signature/archive/refs/heads/master.zip') as response:
                         if response.status == 429:
+                            print(self.count)
                             break
                         else:
                             self.count += 1
+                        if self.count / 1000 == 0:
+                            print(self.count)
                         await response.read()
                 except:
                     continue
@@ -21,3 +26,6 @@ class Task:
         for i in range(30):
             tasks.append(asyncio.create_task(self.task()))
         await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    asyncio.run(Task().start())
